76449eca54b9e04e58f4a98269c4832a8cc48ff2
mane <58097724+online5880@users.noreply.github.com>
2022-07-14 22:06:59 +0900

    웹개발 예제 - 2

--- Web/store.js
+++ Web/store.js
@@ -1,5 +1,7 @@
 let data;
 let card = document.querySelectorAll('.card');
+let bucket;
+let r = 0;
 
 function readTextFile(file, callback) {
     let rawFile = new XMLHttpRequest();
@@ -15,15 +17,18 @@ function readTextFile(file, callback) {
 
 readTextFile("store.json", function(text){
     data = JSON.parse(text);
+    initItem(data);
+});
+
+function initItem(d){
     card.forEach(function (c,idx) {
-        c.querySelector('.card-img-top').src = `${data.products[idx].photo}`;
-        c.children[1].querySelector('.card-title').innerHTML = data.products[idx].title;
-        c.children[1].querySelector('.card-brand').innerHTML = data.products[idx].brand;
-        c.children[1].querySelector('.card-text').innerHTML = `가격 : ${data.products[idx].price}`;
+        c.querySelector('.card-img-top').src = `${d.products[idx].photo}`;
+        c.children[1].querySelector('.card-title').innerHTML = d.products[idx].title;
+        c.children[1].querySelector('.card-brand').innerHTML = d.products[idx].brand;
+        c.children[1].querySelector('.card-text').innerHTML = `가격 : ${d.products[idx].price}`;
         //c.querySelector('.card-text').innerHTML = `${data.products[idx].price}`;
     })
-
-});
+}
 
 document.querySelector('#search').addEventListener('input',function (e){
     if(this.value.length > 0){
@@ -55,26 +60,66 @@ function drag(ev) {
 }
 
 function drop(ev) {
-    console.log('drop')
     ev.preventDefault();
     let data = ev.dataTransfer.getData("text");
     let count = document.createElement('input')
     count.classList.add('count');
     count.type = 'number';
-    count.maxLength = '10';
+    count.max = '10';
     count.min = '1';
     count.value = '1';
+    bucket = ev.target;
+    let value = 1;
     ev.target.appendChild(document.getElementById(data));
     document.getElementById(data).style.height = '300px';
     document.getElementById(data).style.width = '200px';
     document.getElementById(data).style.margin = '10px';
     document.getElementById(data).childNodes[3].removeChild(document.getElementById(data).childNodes[3].lastChild.previousSibling);
     document.getElementById(data).appendChild(count);
-    console.log(count);
-    document.querySelector('.count').addEventListener('input',function (){
-        console.log(this.value);
+    document.querySelector('.count').addEventListener('input',function (e){
+        value = e.data;
+        bucket.querySelectorAll('.card-text').forEach(function (i,idx){
+            r += (parseInt(i.innerHTML.substr(5))*value);
+            Result(r);
+        })
     })
+   bucket.querySelectorAll('.card-text').forEach(function (i,idx){
+       r += (parseInt(i.innerHTML.substr(5))*value);
+       Result(r);
+   })
 }
 
-// TODO 결과값하기
-//document.querySelector('#result').innerHTML =
+function Result(i){
+    document.querySelector('#result').innerHTML = `${i}`;
+        //c.children[1].querySelector('.card-text').innerHTML;
+}
+
+document.querySelector('.purchase').addEventListener('click',function (){
+    document.querySelector('.black-bg').classList.add('show-modal');
+})
+
+document.querySelector('#close').addEventListener('click',function (){
+    document.querySelector('.black-bg').classList.remove('show-modal');
+})
+
+document.querySelector('.black-bg').addEventListener('click',function (e){
+    if(e.target === this){
+        document.querySelector('.black-bg').classList.remove('show-modal');
+        e.stopPropagation();
+    }
+})
+
+document.querySelector('#send').addEventListener('click',function (){
+    document.querySelector('.black-bg').classList.remove('show-modal');
+    bill();
+})
+
+function bill(){
+    let canvas = document.getElementById('canvas');
+    let c = canvas.getContext('2d');
+    let day = new Date();
+    c.font = '20px dotum';
+    c.fillText('영수증', 30, 20);
+    c.fillText(`${day}`, 30, 50);
+    c.fillText('합계 : '+`${r}`,30,80);
+}
